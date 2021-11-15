ae5b1ed9fadf1bc159c244838aef81f0b1021dde
Virsandhu <80320001+Virsandhu@users.noreply.github.com>
2021-11-15 19:30:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+    img = loadImage('https://i.postimg.cc/PJqZsh8Y/Clown-Nose-Download-PNG-Image.png')
+}
+
+function setup(){
+    canvas= createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide()
+
+    posNet= ml5.poseNet(video, modelLoaded)
+    posNet.on('pose',gotPoses)
+
+}
+function modelLoaded(){
+    console.log("Posnet is ready");
+}
+
+function draw(){
+    image(video,0,0,300,300)
+    
+    image(img,noseX,noseY,30,30)
+
+}
+function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+        noseX=results[0].pose.nose.x -13 ;
+        noseY=results[0].pose.nose.y -13 ;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
+        
+    }
+    
+}
+
+
+function take_snapshot(){
+    save('my-nose-filter')
+}
